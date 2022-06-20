b87b529121e82300614773f5ea7bd06a51242ccc
Felipe <lipesantos.gtt@gmail.com>
2022-06-20 16:22:17 -0300

    Trabalhando com variáveis e constantes

---
+++ 02_comentarios.js
@@ -0,0 +1,4 @@
+/*******************************Comentários************************************
+ ** 1_ Evite comentários idiotas **********************************************
+ ** 2_ Procure ter nomes claros ***********************************************
+ ** 3_ comente tudo o que for necessário comentar (contexto do curso online) **/
---
+++ variaveisEConstantes.js
@@ -0,0 +1,16 @@
+var a = 3;
+let b = 4;
+
+var a = 30;
+b = 40;
+
+
+// Em Javascript, ao utilizar o var pode-se declarar a mesma variável várias vezes
+
+a = 300;
+b = 400;
+
+const c = 5;
+
+
+console.log(a,b);
